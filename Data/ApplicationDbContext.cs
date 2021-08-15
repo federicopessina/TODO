7a599c79dd5efa4f78c3d2b11a07c476c7309982
federicopessina <45163627+federicopessina@users.noreply.github.com>
2021-08-15 10:46:18 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TODO.Models;
+
+namespace TODO.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TODO.Models.Bug> Bug { get; set; }
+    }
+}
---
+++ Models/Bug.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODO.Models
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+        public string BugTitle { get; set; }
+        public string BugDetail { get; set; }
+        public Bug()
+        {
+            // needs to be empty
+        }
+    }
+}
